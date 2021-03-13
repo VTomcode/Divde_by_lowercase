951c641cfde64d7077380eeace3de6529fd806df
vtom07 <vivd.vtom07@gmail.com>
2021-03-13 23:05:58 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -3,16 +3,16 @@
 #include "malloc.h"
 int main()
 {
-	char* Sen=NULL;
-	char* Sen_low=NULL;
-	char* Sen_UPP=NULL;
+	char *Sen=NULL;
+	char *Sen_low=NULL;
+	char *Sen_UPP=NULL;
 	int size = 0; 
 	int size_lower_cases = 0;
 	int size_Upper_cases = 0;
 	printf("insert the size  of the sentence");
 	scanf("%d", &size);
 	printf("\n");
-	Sen = (char*) malloc (size * sizeof(char));
+	Sen = (char*) malloc (size+1 * sizeof(char));
 	printf("insert the Sentance - : ");
 	scanf("%s", Sen);
 	for (int i = 0; i < size; i++)
@@ -32,6 +32,7 @@ int main()
 			return 0;
 		}
 	}
+	Sen[size] = '\0';
 	Sen_low = (char*)malloc((size_lower_cases+1) * sizeof(char));
 	Sen_UPP = (char*)malloc((size_Upper_cases+1) * sizeof(char));
 	int j = 0;
@@ -50,13 +51,17 @@ int main()
 		}
 
 	}
-	Sen_low[size_lower_cases] = NULL;
-	Sen_UPP[size_Upper_cases] = NULL;
+	Sen_low[size_lower_cases] = '\0';
+	Sen_UPP[size_Upper_cases] = '\0';
 	printf("The Sentence is  - :");
 	printf(" \n %s \n", Sen);
 	printf("This is the Lowercases in the sentence -");
 	printf(" %s \n", Sen_low);
 	printf("This is the Uppercases in the sentence -");
 	printf(" %s\n", Sen_UPP);
+	free(Sen);
+	free(Sen_low);
+	free(Sen_UPP);
+
 	return 0;
 }
